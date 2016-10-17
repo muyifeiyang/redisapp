e67543fbf4dde7c6aaec90ea4a380992df19d13c
xugang <Administrator@P-GS-XUGANG.xt.gz.csg.local>
2016-10-17 14:50:46 +0800

    add one

--- testApp/src/TestApp.java
+++ testApp/src/TestApp.java
@@ -3,5 +3,6 @@ public class TestApp {
 	public static void main(String[] args) {
 		System.out.println("this is TestApp");
 		System.out.println("this is add");
+		System.out.println("this add one");
 	}
 }
